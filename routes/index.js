4a5dab753a07885ab857812ab24b5e98f27ef250
Soufiane <sfn.elmansoury@gmail.com>
2024-10-02 22:15:03 +0200

    /products route implemented

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
 
-  
-  res.render('index', { title: 'Express' });
-});
+/* GET home page. */
+
+
+
+
+
+router.get('/Products', async (req,res) => {
+  const query = req.query;
+  try {
+    if (!query.trim()) {
+      res.status(400).json({errror:'A query is needed to proceed with the request'})
+    }
+
+    const request = await fetch(`https://api.barcodelookup.com/v3/products?title=${params}&manufacturer=${params}&key=${process.env.API_KEY}`);
+    const data = await response.json({results: request.products})
+    const results = data.map(item => ({
+      title: item.title,
+      brand:item.brand,
+      description:item.description,
+      image:item.image,
+    }))
+
+    res.json({results});
+    console.log('result object filtered', results)
+
+  } catch (error) {
+    console.error('We experienced an issue with /Products endpoint',error)
+    res.status(500).json({error:'Something went wrong'})
+  }
+})
 
 module.exports = router;
