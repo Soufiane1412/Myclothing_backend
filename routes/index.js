baca7f4b98cef999988e5a4585fd154bd05c2a33
Soufiane <sfn.elmansoury@gmail.com>
2024-10-03 03:26:23 +0200

    handling errors

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,24 @@ var router = express.Router();
 
 
 router.get('/Products', async (req,res) => {
+  console.log('querying')
+
   const queryParams = req.query;
     if (Object.keys(queryParams).length===0) {
       res.status(400).json({errror:'A query is needed to proceed with the request'})
     }
   try {
+
     const queryString = Object.entries(queryParams)
     .map(([key, value])=> `${key}=${value}`)
     .join('&');
 
+    console.log('constructed query string', queryString)
+
     const response = await fetch(`https://api.barcodelookup.com/v3/products?${queryString}&key=${process.env.API_KEY}`);
     const data = await response.json()
 
-    console.log(response.json())
+    console.log('API response', response.status)
 
     const results = data.map(item => ({
       title: item.title,
@@ -29,7 +34,6 @@ router.get('/Products', async (req,res) => {
     }))
 
     res.json({results});
-    console.log('result object filtered', results)
 
   } catch (error) {
     console.error('We experienced an issue with /Products endpoint',error)
