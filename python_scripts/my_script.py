5534660e313f6c8a6ddac17c58121c9891962a36
Soufiane <sfn.elmansoury@gmail.com>
2024-10-15 17:47:09 +0200

    scraping data

--- python_scripts/my_script.py
+++ python_scripts/my_script.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='http://books.toscrape.com/'
+
+url='https://books.toscrape.com'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-titles = soup.find_all('h3')
+titles = soup.find_all('h3', 'a')
 for title in titles:
     print(title.text)
