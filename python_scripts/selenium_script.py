47ba4296cf3365b8ae49ab40680554553293dd36
Soufiane <sfn.elmansoury@gmail.com>
2024-10-17 01:50:24 +0200

    extracted dynamic data from BAPE.com using selenium/webdriver/bs4

--- python_scripts/selenium_script.py
+++ python_scripts/selenium_script.py
@@ -1,3 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from bs4 import BeautifulSoup
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+driver.get('https://eu.bape.com/collections/shark-country-pack-clothing')
+
+page_source = driver.page_source
+
+soup = BeautifulSoup(page_source, 'html.parser')
+
+titles = soup.find_all('img')
+
+for title in titles:
+    print(title.get('src'))
