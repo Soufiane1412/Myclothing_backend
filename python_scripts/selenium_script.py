a10b3d7413f096a6373fe59fdd249f5f9fb32fa1
Soufiane <sfn.elmansoury@gmail.com>
2024-10-19 22:51:47 +0200

    swapped Flask for Django framework

--- python_scripts/selenium_script.py
+++ python_scripts/selenium_script.py
@@ -1,17 +1,17 @@
+from flask import Flask, jsonify
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
 
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+app = Flask(__name__)
+@app.route('/scrape')
+def scrape_images():
+    driver = webdriver.Firefox()
+    driver.get('https://eu.bape.com/')
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    images = [img.get('src') for img in soup.find_all('img')]
+    driver.quit()
+    return jsonify(images)
 
-driver.get('https://eu.bape.com/collections/shark-country-pack-clothing')
-
-page_source = driver.page_source
-
-soup = BeautifulSoup(page_source, 'html.parser')
-
-titles = soup.find_all('img')
-
-for title in titles:
-    print(title.get('src'))
+if __name__== '__main__':
+    app.run(debug=True)
