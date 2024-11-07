fa5c34ce0314abb981df3ae02e7f5bf306722a9e
Soufiane <sfn.elmansoury@gmail.com>
2024-11-07 01:07:34 +0100

    debugging runserver

--- clothing_app/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import product_list, scrape_images
-
-urlpatterns =[
-    path('products/', product_list, name='product-list'),
-    # path('scrape/', scrape_images, name='scrape-images'),
-]
--- clothing_app_rebuild/products/views.py
+++ clothing_app_rebuild/products/views.py
@@ -7,8 +7,15 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 from django.http import JsonResponse
-from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
 
 @api_view(['GET'])
 def product_list(request):
@@ -16,11 +23,26 @@ def product_list(request):
     serializer = ProductSerializer(product, many=True)
     return Response(serializer.data)
 
-def scrape_images(request):
+def scrape_images(request): 
     driver = webdriver.Firefox()
-    driver.get('https://www.zalando.fr/mode-homme/')
+    driver.get('https://www.ralphlauren.fr/')
+    # try:
+    #     cookie_button = WebDriverWait(driver, 10).until(
+    #     EC.element_to_be_clickable((By.XPATH, "//button[text()='AUTORISER TOUS LES COOKIES']"))
+    # )
+    #     cookie_button.click()
+    # except:
+    #     print("NO cookie consent popup found, proceeding...")
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
-    images =[img.get('src') for img in soup.find_all('img')]
+    products_images = []
+    product_images =[]
+    for a_tag in soup.find_all('a'):
+        img_tag = a_tag.find('img')
+        if img_tag.get('src'):
+            product_images.append(img_tag)
+            if product_images:
+                products_images.append(product_images)
+
     driver.quit()
-    return JsonResponse(images, safe=False)
+    return JsonResponse(products_images, safe=False)
