c0f7e44a62f6f20e0e09e4c524adaa59249809f0
Soufiane <sfn.elmansoury@gmail.com>
2024-11-12 17:55:22 +0100

    package clashes resolved

--- clothing_app_rebuild/products/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class meta:
-        model = Product
-        fields = ['name', 'brand', 'price', 'image_url']
