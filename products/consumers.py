137f32dcd3b65aeb56758046bdbf82ec5d16633b
Soufiane <sfn.elmansoury@gmail.com>
2024-11-21 18:03:19 +0100

    created first websocket consumer

---
+++ products/consumers.py
@@ -0,0 +1,22 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        # Join notifications gorup
+        await self.channel_layer.group_add(
+            'notifications', # Group name
+            self.order_updates # Unique channel name
+        )
+        await self.accept() # Accept websocket connection
+
+    async def disconnect(self, close_code):
+        # Leave notifications group
+        await self.channel_layer.group_discard(
+            'notifications',
+            self.order_updates
+        )
+    async def send_notifications(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['Hello everyone it is my first websocket notificationnotification 🙋🏻‍♂️👨🏻‍💻📮']
+        }))
