44b612dbd64b86286dffb14811033c3e00820037
Soufiane <sfn.elmansoury@gmail.com>
2024-11-29 19:19:36 +0100

    feat: added token serializer

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,12 @@ class ProductSerializer(serializers.ModelSerializer):
     class meta:
         model = Product
         fields = ['name', 'brand', 'price', 'image_url']
+
+
+class MyTokenObtainPairSerializer(TokenObtainPariSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+
+        return token
