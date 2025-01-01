94edd511ffc85d3028168441e7fb24dc33caf512
Soufiane <sfn.elmansoury@gmail.com>
2025-01-01 21:23:35 +0100

    fix: typo fixed

--- clothing_app_rebuild/settings.py
+++ clothing_app_rebuild/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,6 +65,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'clothing_app_rebuild.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 TEMPLATES = [
     {
