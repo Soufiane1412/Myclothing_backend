4b1f33dd5bdd39970a014777326aef030a57b3e6
Soufiane <sfn.elmansoury@gmail.com>
2025-01-05 20:06:39 +0100

    feat: jwt token configured

--- clothing_app_rebuild/settings.py
+++ clothing_app_rebuild/settings.py
@@ -73,6 +73,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKEN': True
+
 }
 
 TEMPLATES = [
