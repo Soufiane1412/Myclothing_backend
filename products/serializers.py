9dd9401c62830416ffe1c9e1f7a4a9fa37995e99
Soufiane <sfn.elmansoury@gmail.com>
2025-01-06 16:45:45 +0100

    feat: token serializer implementations

--- products/serializers.py
+++ products/serializers.py
@@ -29,18 +29,56 @@ class UserSerializer(serializers.ModelSerializer):
     UserSerializer converts User model instances to/from JSON.
     It inherits from ModelSerializer which provides default create/update operations
     """
-    # write_only = True ensures password isn't included in responses
+    # Additional field not in the model
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-
+        model = User # specifies which model to serialize
+        fields = ('id', 'username', 'email', 'password', 'password_confirm')
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+    
+    def validate(self, data):
+        """
+        Custom validation method.
+        vaidated_data is created after this validation passes.
+        """
+        if data.get('password') != data.get('password_confirm'):
+            raise serializers.ValidationError("Passwords don't match 🤔 try again")
+        return data
     
     def create(self, validated_data):
+
+        """
+        Override the create method to properly handle password hashing.
+        validated_data: Dictionary of field values that passed validation
+        """
         # Never store raw passwords! Django handles hashing
         user = user.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''), # Email optional
             password=validated_data['password']
         )
+        return user
+    
+    """
+    #1 When a POST request comes in with JSON data:
+    {
+        "username": "John_Doe",
+        "email": "John@example.com",
+        "password": "securepass123",
+        "password_confirm": "securepass123"
+    }
+
+    The flow is:
+
+    1. Conversion from JSON to Python Dictionary (automatic)
+    2. Validate() checks passwords match
+    3. create() is called with validated_data
+    4. User instance is created with a hashed password
+    5. response is serialized back to JSON (without password field)
+    """
+
+
+
