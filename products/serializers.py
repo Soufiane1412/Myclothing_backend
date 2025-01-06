c46b7a672618c9a0814f5eabc4b9cb7c561524d9
Soufiane <sfn.elmansoury@gmail.com>
2025-01-06 13:08:02 +0100

    feat: class setup & token serializer

--- products/serializers.py
+++ products/serializers.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import Token
 from .models import Product
+from django.contrib.auth.models import User
 
 class ProductSerializer(serializers.ModelSerializer):
     class meta:
@@ -21,3 +22,25 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 # Initialised the token view
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+
+    """
+    UserSerializer converts User model instances to/from JSON.
+    It inherits from ModelSerializer which provides default create/update operations
+    """
+    # write_only = True ensures password isn't included in responses
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
+    
+    def create(self, validated_data):
+        # Never store raw passwords! Django handles hashing
+        user = user.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''), # Email optional
+            password=validated_data['password']
+        )
