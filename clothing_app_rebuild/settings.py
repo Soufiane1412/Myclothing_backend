5f150ae278c9f024c4ed61e4864161db1113d6da
Soufiane <sfn.elmansoury@gmail.com>
2025-02-22 00:10:14 +0100

    feat: cors url added to allow request from front

--- clothing_app_rebuild/settings.py
+++ clothing_app_rebuild/settings.py
@@ -153,6 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Add your React Native development server
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
     "exp://localhost:19000",  # For Expo development
     "http://localhost:19006",  # For web browser testing
 ]
