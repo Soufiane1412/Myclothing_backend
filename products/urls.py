fa3c2023c0c0b7c9167d0136fb3cf2fde8cf77d2
Soufiane <sfn.elmansoury@gmail.com>
2025-02-24 20:13:59 +0100

    feat:updated url patterns

--- products/urls.py
+++ products/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from .views import product_list, scrape_images, register
 
 # Token JWT import 
-from .views import MyTokenObtainPairView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns =[
     path('api/products/', product_list, name='product-list'),
     path('api/products/scrape/', scrape_images, name='scrape-images'),
-    path('api/auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/register/', register,  name='register'),
 
