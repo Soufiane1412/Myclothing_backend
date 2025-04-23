8354a89f07ca41ca8530fb61b38b50b923fb476f
Soufiane <sfn.elmansoury@gmail.com>
2025-04-23 20:57:22 +0200

    fix:missing JWT endpoints in project level urls

--- clothing_app_rebuild/urls.py
+++ clothing_app_rebuild/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt import TokenobtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('api/', include('products.urls')),
+    path('api/auth/token/', TokenobtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name="token_refresh")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
